c7513e53e3e17133caf7c85d8015a7ce9414a555
BirkasTivadar <birkastivadar@gmail.com>
2021-03-14 21:56:47 +0100

    Gyakorlati feladat 1 - JobDispatcher kész Gyakorlati feladat 2 - Deque implementáció kész

---
+++ src/main/java/collectionsqueue/collectionsdeque/JobQueue.java
@@ -0,0 +1,38 @@
+package collectionsqueue.collectionsdeque;
+
+import collectionsqueue.job.Job;
+import collectionsqueue.job.NoJobException;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+
+public class JobQueue {
+
+    public Deque<Job> addJobByUrgency(Job... jobs) {
+        Deque<Job> result = new ArrayDeque<>();
+        for (Job job : jobs) {
+            if (job.isUrgent()) {
+                result.addFirst(job);
+            } else {
+                result.addLast(job);
+            }
+        }
+        return result;
+    }
+
+    public Job dispatchUrgentJob(Deque<Job> jobs) {
+        if (jobs.isEmpty()) {
+            throw new NoJobException("No job available, get a rest!");
+        }
+        return jobs.pollFirst();
+    }
+
+    public Job dispatchNotUrgentJob(Deque<Job> jobs) {
+        if (jobs.isEmpty()) {
+            throw new NoJobException("No job available, get a rest!");
+        }
+        return jobs.pollLast();
+    }
+
+}
